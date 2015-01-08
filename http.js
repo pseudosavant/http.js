03d8417e8b17a02c02252605e6d721e074d73111
paul@ellisfoundation.com <paul@ellisfoundation.com>
2015-01-07 22:29:58 -0800

    Minor changes to make code fit in my project's environment better. - Wrapped in IIFE - Fixed `options`  so that it isn't defined twice - Fixed random JSHint warnings (braces, semicolons)

--- http.js
+++ http.js
@@ -1,46 +1,52 @@
-var http = {
-    DEFAULTS: {
-        async: true,
-        contentType: "text/plain",
-        data: null,
-        headers: {},
-        method: "GET",
-        onerror: function() {},
-        onload: function() {},
-        onreadystatechange: function() {},
-        url: "./",
-    },
-    request: function(options) {
-        // Get options and initialize request with URL
-        var options = this.options(options);
-        var req = new XMLHttpRequest();
-        req.onload = options.onload;
-        req.open(options.method, options.url, options.async);
+(function (global) {
+    'use strict';
 
-        // Request headers
-        for (var key in options.headers) {
-            if (options.headers.hasOwnProperty(key))
-                req.setRequestHeader(key, options.headers[key]);
-        }
-        req.setRequestHeader('Content-Type', options.contentType);
+    global.http = {
+        DEFAULTS: {
+            async: true,
+            contentType: "text/plain",
+            data: null,
+            headers: {},
+            method: "GET",
+            onerror: function () { },
+            onload: function () { },
+            onreadystatechange: function () { },
+            url: "./",
+        },
+        request: function (options) {
+            // Get options and initialize request with URL
+            options = this.options(options);
+            var req = new XMLHttpRequest();
+            req.onload = options.onload;
+            req.open(options.method, options.url, options.async);
 
-        // Send request and return
-        req.send(options.data);
-        return req;
-    },
-    options: function(options) {
-        var merged = {}, defaults = this.DEFAULTS;
-        for (var key in defaults) {
-            if (defaults.hasOwnProperty(key))
-                merged[key] = options[key] || defaults[key];
+            // Request headers
+            for (var key in options.headers) {
+                if (options.headers.hasOwnProperty(key)) {
+                    req.setRequestHeader(key, options.headers[key]);
+                }
+            }
+            req.setRequestHeader('Content-Type', options.contentType);
+
+            // Send request and return
+            req.send(options.data);
+            return req;
+        },
+        options: function (options) {
+            var merged = {}, defaults = this.DEFAULTS;
+            for (var key in defaults) {
+                if (defaults.hasOwnProperty(key)) {
+                    merged[key] = options[key] || defaults[key];
+                }
+            }
+            return merged;
+        },
+        get: function (options) {
+            return this.request(options);
+        },
+        post: function (options) {
+            options.method = "POST";
+            return this.request(options);
         }
-        return merged;
-    },
-    get: function(options) {
-        return this.request(options);
-    },
-    post: function(options) {
-        options.method = "POST";
-        return this.request(options);
-    }
-}
+    };
+})(this);
